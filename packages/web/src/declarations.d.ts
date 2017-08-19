b654617038209f25d79b1fcdf8a688d424cb1f6d
Alex Gherghisan <alex@alexghr.me>
2017-08-19 15:17:59 +0100

    Use a single event source connection to signaling server

--- packages/web/src/declarations.d.ts
+++ packages/web/src/declarations.d.ts
@@ -4,10 +4,17 @@ declare module 'signalhub' {
 
   namespace signalhub {
     interface SignalHub {
-      subscribe(channel: string): {addListener: Function, removeListener: Function};
+      subscribe(channel: string): EventListener;
       broadcast(channel: string, data: any, cb?: Function): void;
       close(cb?: Function): void;
     }
+
+    interface EventListener {
+      addListener: (evt: string, callback: EventHandler) => void;
+      removeListener: (evt: string, callback: EventHandler) => void;
+    }
+
+    type EventHandler = (event: object) => any;
   }
 
   export = signalhub;
--- packages/web/src/store/api/signal.ts
+++ packages/web/src/store/api/signal.ts
@@ -4,11 +4,16 @@ import { Slug } from '../state';
 export default class Signal {
 
   private _hubConn: signalhub.SignalHub | null = null;
+  private _subMap: Map<string, signalhub.EventListener> = new Map();
 
   constructor(private signalUrl: string) {}
 
   listenForMessages(slug: Slug, cb: (evt: object) => void) {
-    const stream = this.connection.subscribe(slug);
+    if (!this._subMap.has(slug)) {
+      this._subMap.set(slug, this.connection.subscribe(slug));
+    }
+
+    const stream = this._subMap.get(slug)!;
 
     const eventHandler = (data: object) => {
       cb(data);
